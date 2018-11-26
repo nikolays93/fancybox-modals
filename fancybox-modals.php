e00a340ccc2a9d25a8ad2e110ff56bffdf66a606
Nickolays93 <nikolays93@ya.ru>
2018-11-26 16:04:39 +0400

    change DOMAIN (Shortocde) name

--- fancybox-modals.php
+++ fancybox-modals.php
@@ -10,7 +10,7 @@
  * Author EMAIL: NikolayS93@ya.ru
  * License: GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: fancybox-modal
+ * Text Domain: fb-modal
  * Domain Path: /languages/
  */
 
